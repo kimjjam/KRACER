64da648902e588ab9d985e4e3f779003985dbf4c
kimjjam <94438160+kimjjam@users.noreply.github.com>
2025-08-21 19:41:10 +0900

    Update socket.ts

--- kracker/src/api/socket.ts
+++ kracker/src/api/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
 const SOCKET_URL =
-  process.env.REACT_APP_SOCKET_URL || "http://localhost:3001";
+  process.env.REACT_APP_SOCKET_URL || "https://kracer-server.onrender.com";
 
 export type PlayersState = Record<string, { x: number; y: number }>;
 
